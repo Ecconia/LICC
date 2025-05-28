Ecconia/LICC

commit 1cfbb28ae8673bb945a40b5f713c4cb5dfb225db

Create autoexec if it doesn't already exist

LICC/API/Filesystem.cs
@@ -9,6 +9,7 @@ namespace LICC.API
     public interface IFileSystem
     {
         bool FileExists(string path);
+        void CreateFile(string path);
         StreamReader OpenRead(string path);
     }
 
@@ -27,6 +28,8 @@ namespace LICC.API
         private string FilePath(string path) => Path.Combine(RootPath, path);
 
         public bool FileExists(string path) => File.Exists(FilePath(path));
+
+        public void CreateFile(string path) => File.Create(FilePath(path)).Close();
 
         public StreamReader OpenRead(string path)
             => new StreamReader(File.OpenRead(FilePath(path)), Encoding.UTF8, true, 4096, false);

LICC/CommandConsole.cs
@@ -90,8 +90,15 @@ namespace LICC
         /// </summary>
         public void RunAutoexec()
         {
-            if (FileSystem?.FileExists("autoexec.lsf") ?? false)
-                Shell.ExecuteLsf("autoexec.lsf");
+            const string autoExecFileName = "autoexec.lsf";
+
+            if (FileSystem != null)
+            {
+                if (FileSystem.FileExists(autoExecFileName))
+                    Shell.ExecuteLsf(autoExecFileName);
+                else
+                    FileSystem.CreateFile(autoExecFileName);
+            }
         }
 
         public void SwitchFrontend(Frontend frontend)

commit a6dee798b69b3cdec278187bc15288bf9b005c9c

Fix LexerTest
Was broken by commit (1cfbb28ae8673bb945a40b5f713c4cb5dfb225db). Simply implementing an empty method is sufficient as a fix.

LICC.Test/Parsing/LexerTest.cs
@@ -1,4 +1,4 @@
-﻿using LICC.API;
+using LICC.API;
 using LICC.Internal.LSF.Parsing;
 using LICC.Internal.LSF.Parsing.Data;
 using NUnit.Framework;
@@ -23,6 +23,11 @@ namespace LICC.Test.Parsing
             }
 
             public bool FileExists(string path) => true;
+
+            public void CreateFile(string path)
+            {
+                // Nothing to do here. This class represents a read-only file that does not have to be created.
+            }
 
             public StreamReader OpenRead(string path)
             {